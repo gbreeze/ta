a63a9a21f27561c460b973091efe6132b5991fa9
Kevin Johnson <appliedmathkj@gmail.com>
2018-09-18 11:14:15 -0700

    BasePandasObject Creation

--- ta/_extension.py
+++ ta/_extension.py
@@ -4,3 +4,16 @@ import numpy as np
 import pandas as pd
 
 from pandas.core.base import PandasObject
+
+class BasePandasObject(PandasObject):
+    def __init__(self, data, **kwargs):
+        if data.empty:
+            return None
+        
+        if len(data.columns):
+            self._data = data
+        else:
+            raise AttributeError(f"[X] Not enough columns!")
+    
+    def __call__(self, kind, *args, **kwargs):
+        raise NotImplementedError()
